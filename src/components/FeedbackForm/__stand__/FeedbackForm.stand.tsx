75b0aa10d59b1ea5830c74879d652b55df2bf28e
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-31 15:02:03 +0300

    docs(documentation): add doc for stand (#13)
    consta-design-system/uikit#2568

--- src/components/FeedbackForm/__stand__/FeedbackForm.stand.tsx
+++ src/components/FeedbackForm/__stand__/FeedbackForm.stand.tsx
@@ -4,7 +4,9 @@ export default createStand({
   title: 'FeedbackForm',
   id: 'FeedbackForm',
   group: 'components',
-  description: 'Этот адаптер стилизует диаграмму Ганта .',
+  description: 'Виджет обратной связи.',
   version: '0.2.2',
   status: 'stable',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D0%253A1',
 });
