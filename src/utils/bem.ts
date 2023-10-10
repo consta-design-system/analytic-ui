404226fc74f184d42c8708faefe97554865c71ad
Maxim Kolesnikov <socialfox33@gmail.com>
2023-10-10 11:40:02 +0300

    chore(utils): added status classNames
    closes consta-design-system/uikit#2658

--- src/utils/bem.ts
+++ src/utils/bem.ts
@@ -3,3 +3,10 @@ import { withNaming } from '@bem-react/classname';
 const reactBemNaming = { n: 'aui--', e: '-', m: '_', v: '_' };
 
 export const cn = withNaming(reactBemNaming);
+
+export const withPrefix = (prefix: string) =>
+  withNaming({ ...reactBemNaming, n: prefix + reactBemNaming.n });
+
+export const cnCanary = withPrefix('canary');
+
+export const cnDeprecated = withPrefix('deprecated');
